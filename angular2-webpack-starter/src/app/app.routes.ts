1386a7664c9ad9bb4f8d18bd967041e15c5d45e7
didoogan <tzd0409@gmail.com>
2017-01-04 09:18:38 +0200

    dich

---
+++ angular2-webpack-starter/src/app/app.routes.ts
@@ -0,0 +1,16 @@
+import { Routes, RouterModule } from '@angular/router';
+import { HomeComponent } from './home';
+import { AboutComponent } from './about';
+import { NoContentComponent } from './no-content';
+import { CategoryComponent } from './category/category.component';
+
+import { DataResolver } from './app.resolver';
+
+export const ROUTES: Routes = [
+  { path: '',      component: HomeComponent },
+  { path: 'home',  component: HomeComponent },
+  { path: 'about', component: AboutComponent },
+  { path: 'detail', loadChildren: './+detail/index#DetailModule'},
+  { path: 'category', component: CategoryComponent },
+  { path: '**',    component: NoContentComponent },
+];
---
+++ angular2-webpack-starter/src/app/category/category.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+    selector: 'category',
+    templateUrl: './category.component.html'
+    // template: `
+    //     <h2>Component List: </h2>
+    //     <h3>Dich detected</h3>
+    // `
+})
+export class CategoryComponent {}
